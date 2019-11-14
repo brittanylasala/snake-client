c1947b61f1b9b62b14b244dc54a0e326527b9de9
Brittany Lasala <brittanylasala@gmail.com>
2019-11-14 20:22:11 +0000

    Began setup for standard input and created ctrl+c ability to exit program

--- play.js
+++ play.js
@@ -1,3 +1,26 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+const handleUserInput = function(command) {
+  if (command === '\u0003') { // if ctrl+c is pressed
+    process.exit();
+  };
+};
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.on('data', (data) => {
+    handleUserInput(data);
+  });
+  stdin.resume();
+  return stdin;
+}
+
+setupInput();
