967da0b799a4140483d6c417877dbaa5b94ca436
Brittany Lasala <brittanylasala@gmail.com>
2019-11-14 20:39:18 +0000

    refactored and created input module

---
+++ input.js
@@ -0,0 +1,26 @@
+const handleUserInput = function(command) {
+  if (command === '\u0003') { // if ctrl+c is pressed
+    process.exit();
+  };
+};
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.on('data', (data) => {
+    handleUserInput(data);
+  });
+  stdin.resume();
+  return stdin;
+}
+
+let playerInput = {
+  setupInput
+}
+
+module.exports = playerInput;
--- play.js
+++ play.js
@@ -1,26 +1,7 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
-
-const handleUserInput = function(command) {
-  if (command === '\u0003') { // if ctrl+c is pressed
-    process.exit();
-  };
-};
-
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.on('data', (data) => {
-    handleUserInput(data);
-  });
-  stdin.resume();
-  return stdin;
-}
 
 setupInput();
