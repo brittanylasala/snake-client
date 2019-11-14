a864b9f308bfbe2eaa4bd5b32826286c91d1eaf1
Brittany Lasala <brittanylasala@gmail.com>
2019-11-14 21:36:51 +0000

    snake game now takes directin input to move around screen and play game

--- input.js
+++ input.js
@@ -1,6 +1,16 @@
+let connection;
+
 const handleUserInput = function(command) {
   if (command === '\u0003') { // if ctrl+c is pressed
     process.exit();
+  } if (command === 'w') {
+    connection.write('Move: up');
+  } if (command === 'a') {
+    connection.write('Move: left');
+  } if (command === 's') {
+    connection.write('Move: down');
+  } if (command === 'd') {
+    connection.write('Move: right');
   };
 };
 
@@ -8,7 +18,8 @@ const handleUserInput = function(command) {
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
